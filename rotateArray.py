13f0532e2f45423fb4395d81cfde445d324bb72a
dhanush <bhatdhanush79@gmail.com>
2023-01-06 20:10:03 +0530

    Spelling mistake correction

--- rotateArray.py
+++ rotateArray.py
@@ -1,6 +1,6 @@
 def rotate(array,k):
     return (array[k:] + array[:k])  
 
-array = list(map(int,input("Etner the array: \n").split(' '))) # Please enter the array of numbers in a single line with a space in between
+array = list(map(int,input("Enter the array: \n").split(' '))) # Please enter the array of numbers in a single line with a space in between
 k = int(input("Enter k: ").strip())                            # Enter k lower than or equal to the length of the array only
 print(rotate(array,k))
